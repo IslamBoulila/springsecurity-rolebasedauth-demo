fa1931cc416a3b447698d7b5aed721190bcf1d98
Islam Boulila <islamboulila@gmail.com>
2022-03-06 16:01:45 +0100

    [Method security config: clean code]

--- src/main/java/com/security/islam/security/config/security/MethodSecurityConfig.java
+++ src/main/java/com/security/islam/security/config/security/MethodSecurityConfig.java
@@ -2,28 +2,23 @@ package com.security.islam.security.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+
 import org.springframework.security.access.expression.method.ExpressionBasedPreInvocationAdvice;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.prepost.PreInvocationAuthorizationAdviceVoter;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.access.vote.AuthenticatedVoter;
-import org.springframework.security.access.vote.RoleHierarchyVoter;
 import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
+
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -53,14 +48,13 @@ public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
     }
 
     /**
+     * How to change the default access decision manager in Spring security
      * AccessDecisionManager makes a final access control (authorization) decision.
      * Overriding this method is necessary for role hierarchy, customized security methods..
      * @return
      */
     @Override
     protected AccessDecisionManager accessDecisionManager() {
-        //AffirmativeBased manager = (AffirmativeBased)super.accessDecisionManager() ;
-        //  manager.getDecisionVoters().clear();
 
         List<AccessDecisionVoter<? extends Object>> decisionVoters = new ArrayList<>();
 
@@ -71,11 +65,6 @@ public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
         decisionVoters.add(roleVoter); //Add the customized RoleVoter Bean
         decisionVoters.add(new AuthenticatedVoter()); //It is necessary to add this one when we override the default AccessDecisionManager
 
-      /*  manager.getDecisionVoters().add(roleVoter);
-        manager.getDecisionVoters().add(new AuthenticatedVoter());
-
-        manager.getDecisionVoters().add(new PreInvocationAuthorizationAdviceVoter(expresionAdvice));
-        */
        return new AffirmativeBased(decisionVoters);
 
     }
@@ -86,18 +75,4 @@ public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
        return  webExpressionVoter;
     }
 */
-
-    /**
-     * How to change the default access decision manager in Spring security
-     * @return
-     */
-  /*  @Override
-    protected AccessDecisionManager accessDecisionManager() {
-        List < AccessDecisionVoter << ? extends Object >> decisionVoters = Arrays.asList(
-                new AuthenticatedVoter(),
-                new RoleVoter(),
-                new WebExpressionVoter()
-        );
-        return new UnanimousBased(decisionVoters);
-    }*/
 }
